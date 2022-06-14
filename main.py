d6b2bcf20fc164727f2dcb13167184fd600a2412
Afonso Pinto <afonso.pinto2710@hotmail.com>
2022-06-14 23:10:36 +0100

    Menu with validation

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("teste")
+import os
+
+option = []
+
+def intro():
+    os.system('cls')
+    print("""
+    MENU
+    -----------------------------------------------------------------------                                                             
+    1   Add option
+    2   Clear options
+    3   Randomize
+    0   Exit
+    -----------------------------------------------------------------------
+    """)
+    var= input("\tChoice : ")
+
+    if var == "1" :
+        print(1)
+
+        os.system('pause')    
+        intro()
+        
+    #Exit
+    elif var == "0" :
+        exit("\tBye\n")
+    
+    #Invalid choice 
+    else:
+        print("\n\tInvalid choice\n")
+        
+        os.system('pause')    
+        intro()
+
+    
+
+intro()
